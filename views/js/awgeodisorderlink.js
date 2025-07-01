effe85ffeb08ddc17eadd5fba062b9722c5c9ad8
Axel Paillaud <contact@axelweb.fr>
2025-07-01 17:23:12 +0200

    init js script to fetch controller and update status

--- views/js/awgeodisorderlink.js
+++ views/js/awgeodisorderlink.js
@@ -1,3 +1,23 @@
-const updateBtn = document.getElementById('geodis-link-update-status');
 
-console.log(updateBtn);
+document.addEventListener('DOMContentLoaded', () => {
+  const updateBtn = document.getElementById('geodis-link-update-status');
+
+  updateBtn.addEventListener('click', () => {
+    fetch(awGeodisOrderLinkUpdateStateOrderAjaxControllerUri, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        orderId: orderId,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  });
+});
